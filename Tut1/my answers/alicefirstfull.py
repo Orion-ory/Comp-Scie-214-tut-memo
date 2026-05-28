2bfecf598f3cfec31ea421b7e11be188383033c9
Andile Ntuli <andilayshelby@gmail.com>
2026-05-28 07:35:55 +0200

    created the full version of alice first

---
+++ Tut1/my answers/alicefirstfull.py
@@ -0,0 +1,74 @@
+import sys
+import random
+import stdrandom
+import stdio
+import math
+
+
+def tush(a: list, b: str):
+    i = 0
+    stdio.write("    " + b + ":  ")
+    for i in range(len(a)):
+        stdio.write(a[i])
+    return
+
+
+def main():
+
+    num_sims = int(sys.argv[1])
+    alicefirst = 0
+    probability = 0.0
+    i = 0
+    G = len(sys.argv) > 2
+
+    for i in range(num_sims):
+        A_prev = "A"
+        A_cur = "B"
+        B_prev = "C"
+        B_cur = "D"
+        A = []
+        B = []
+        A_win = False
+        B_win = False
+
+        while A_win != True and B_win != True:
+
+            if not A_win:
+                A_prev = A_cur
+                A_cur = random.choice(["H", "T"])
+                A += [A_cur]
+
+            if not B_win:
+                B_prev = B_cur
+                B_cur = random.choice(["H", "T"])
+                B += [B_cur]
+
+            if B_prev == "H" and B_cur == "T":
+                B_win = True
+            if A_cur == A_prev and A_cur == "H":
+                if B_win == False and A_win == False:
+                    alicefirst += 1
+                    A_win = True
+                else:
+                    A_win = True
+        if G:
+            tush(A, "Alice")
+            stdio.write("  ")
+            tush(B, "Bob")
+            stdio.writeln()
+
+        A_cur = "A"
+        A_prev = "B"
+        B_cur = "C"
+        B_prev = "D"
+        A_win = False
+        B_win = False
+        A = []
+        B = []
+
+    probability = alicefirst / num_sims
+    stdio.writeln(str(probability))
+
+
+if __name__ == "__main__":
+    main()
